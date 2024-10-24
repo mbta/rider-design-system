c50c40520949fb984f8ac391842769f0deba842d
Cristen Jones <cjones3@mbta.com>
2024-10-24 16:44:24 -0400

    wip: corrected tokens

--- tokens/build.js
+++ tokens/build.js
@@ -1,11 +1,18 @@
 import { register } from '@tokens-studio/sd-transforms';
 import StyleDictionary from 'style-dictionary';
 import { getTailwindFormat } from 'sd-tailwindcss-transformer';
+import { kebabCase } from 'change-case';
 
 register(StyleDictionary);
 
 const tailwindConfig = {
   hooks: {
+    filters: {
+      'base': token => {
+        const fileName = token.path[0];
+        return fileName == "Base/Mode 1" || fileName == "MBTA System/Value";
+      },
+    },
     formats: {
       tailwindFormat: ({ dictionary }) => {
         return getTailwindFormat({
@@ -20,24 +27,33 @@ const tailwindConfig = {
           }
         })
       }
+    },
+    transforms: {
+      'removeFileName': {
+        type: 'attribute',
+        transitive: true,
+        transform: token => {
+          delete token.attributes.category
+          return token.attributes;
+        },
+      }
     }
   },
-  source: [
-    'tokens/Base/Mode 1.json',
-    `tokens/Mbta System/Value.json`,
-  ],
+  source: ["tokens/tokens.json"],
   preprocessors: ['tokens-studio'],
   platforms: {
     tailwind: {
       transforms: [
         'attribute/cti',
+        'removeFileName',
         'name/kebab'
       ],
       buildPath: "dist/",
       files: [
         {
           destination: "tailwind.config.js",
-          format: 'tailwindFormat'
+          format: 'tailwindFormat',
+          filter: 'base'
         }
       ]
     }
@@ -46,17 +62,38 @@ const tailwindConfig = {
 
 const themeCSSConfigs = ["Light", "Dark"].map(theme =>
 ({
-  include: ['tokens/Base/Mode 1.json'],
-  source: [`tokens/Semantic/${theme}.json`],
+  source: [
+     "tokens/tokens.json"
+  ],
+  hooks: {
+    filters: {
+      'theme': token => {
+        const fileName = token.path[0];
+        if (fileName == "Base/Mode 1") return true;
+        return fileName == `Semantic/${theme}` || fileName == `Components/${theme}`;
+      },
+    },
+    transforms: {
+      'removeFileName': {
+        type: `name`,
+        transitive: true,
+        transform: (token) => {
+          const [category, ...tokenPath] = token.path;
+          return kebabCase(tokenPath.join(' '))
+        },
+      }
+    }
+  },
   preprocessors: ['tokens-studio'],
   platforms: {
     css: {
       transformGroup: 'tokens-studio',
-      transforms: ['name/kebab'],
+      transforms: ['removeFileName'],
       buildPath: 'dist/',
       files: [{
         destination: `variables.${theme.toLowerCase()}.css`,
         format: 'css/variables',
+        filter: 'theme'
       }],
     }
   }
